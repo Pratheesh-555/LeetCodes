bd5f18038794a4acb3b1f8e07886b6716cfb79cf
Pratheesh-555 <127003195@sastra.ac.in>
2025-02-15 23:32:46 +0530

    added leetcode solution

---
+++ LeetCode_2698.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    int stringToNumber(string s)
+    {
+        int num = 0;
+        for (char c : s)
+        {
+            num = num * 10 + (c - '0');
+        }
+        return num;
+    }
+
+    bool canPartition(string num, int target, int index = 0, int tempSum = 0)
+    {
+        if (index == num.length())
+            return tempSum == target;
+
+        for (int i = index; i < num.length(); i++)
+        {
+            int part = stringToNumber(num.substr(index, i - index + 1));
+            if (canPartition(num, target, i + 1, tempSum + part))
+                return true;
+        }
+        return false;
+    }
+
+    int punishmentNumber(int n)
+    {
+        int total = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            string num = to_string(i * i);
+            if (canPartition(num, i))
+                total += (i * i);
+        }
+        return total;
+    }
+};
+
+int main()
+{
+    Solution S;
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    cout << "Punishment Number: " << S.punishmentNumber(n) << endl;
+    return 0;
+}

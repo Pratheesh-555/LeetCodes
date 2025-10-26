3cbd07075afc81354a77ace73fe45bf3c11ff875
Pratheesh-555 <127003195@sastra.ac.in>
2025-10-27 01:19:37 +0530

    random

---
+++ LeetCode_14.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        string res = "";
+        for(int i = 0; i < strs[0].size(); i++){
+            if(strs[0][i] != strs[strs.size()-1][i]) break;
+            res += strs[0][i];
+        }
+        return res;
+    }
+};

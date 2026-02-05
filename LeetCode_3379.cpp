3f427022dc50b62c6a85447d940aff74a8a80489
Pratheesh-555 <127003195@sastra.ac.in>
2026-02-05 23:51:33 +0530

    daily ques

---
+++ LeetCode_3379.cpp
@@ -0,0 +1,43 @@
+// class Solution {
+// public:
+//     vector<int> constructTransformedArray(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> res;
+//         for(int i= 0; i < n; i++){
+//             if(nums[i] > 0){
+//                 res.push_back(nums[(i+nums[i])%n]);
+//             }
+//             else if(nums[i] < 0){
+//                 res.push_back(nums[(i-nums[i])%n]);
+//             }
+//             else{
+//                 res.push_back(nums[i]);
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& A) {
+        int n = A.size();
+        int OFFSET = 1 << 7;
+        int MASK = (1 << 8) - 1;
+
+        for (int i = 0; i < n; i++)
+            A[i] += OFFSET;
+
+        for (int i = 0; i < n; i++) {
+            int cur = A[i] - OFFSET;
+            int idx = ((i + cur) % n + n) % n;
+            int val = A[idx] & MASK;
+            A[i] |= val << 8;
+        }
+
+        for (int i = 0; i < n; i++)
+            A[i] = (A[i] >> 8) - OFFSET;
+
+        return A;
+    }
+};
